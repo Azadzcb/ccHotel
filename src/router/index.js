11904223eae1939a234d1633d830bfce9f8ac100
Azadzcb <757900482@qq.com>
2019-11-05 12:53:17 +0800

    载入页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import login from '../views/login.vue'
 import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
@@ -7,17 +8,17 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: login
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/login.vue
@@ -0,0 +1,29 @@
+
+<template>
+    <div class="page-login">
+        <el-button type="primary">主要按钮</el-button>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    name:'login',
+    data(){
+        return{
+            
+        }
+    },
+    methods: {
+        
+    }
+}
+</script>
+
+<style>
+    .page-login{
+        background-color: #f0f2f5;
+        height: 100%;
+    }
+</style>
